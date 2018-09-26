b7fefaa3c9de54215fdf21a5d13ce0a0959d53b5
singaltanmay <tanmaysingal@icloud.com>
2018-09-26 21:33:42 +0530

    learning javascript

---
+++ portfolio/learning_javascript/script.js
@@ -0,0 +1,21 @@
+var names = ["Tanmay", "Harshul"];
+
+console.log(names[0]);
+
+for (var x = 1; x <= 10; x++){
+    console.log(x);
+}
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+var tui = 0
+
+do {
+    console.log(tui++);
+} while (tui < 9);
+
+if (true && 1) {
+    console.log("true and 1 are the same thing");
+}
